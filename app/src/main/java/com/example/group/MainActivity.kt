bd041168d178a834de0f22bd5aac0949e24f3b2f
jiiione <jwpdream00@gmail.com>
2024-06-25 18:34:01 -0400

    cal

--- app/src/main/java/com/example/group/MainActivity.kt
+++ app/src/main/java/com/example/group/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.group
 
 import android.os.Bundle
+import android.widget.CalendarView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var calendarView: CalendarView
+    private var totalTasks: Int = 0
+    private var completedTasks: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
@@ -16,5 +24,8 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        calendarView = findViewById(R.id.calendarView)
     }
 }
